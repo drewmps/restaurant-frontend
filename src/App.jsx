2469a166e81e783471c2fe1ca8e94a0b509672cc
drewmps <andrewmarco97@gmail.com>
2025-03-18 15:40:39 +0700

    refactor: memisahkan halaman login menjadi component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Preloader from "./components/Preloader";
 import DetailProduct from "./pages/DetailProduct";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -34,74 +35,9 @@ function App() {
         </button>
       </header>
       {/* End Navbar */}
-      {/* Login Section */}
-      <section className="container" id="login-section">
-        <div className="row">
-          <div className="col-12 text-center">
-            <h1 className="mb-3 mt-5">Login</h1>
-          </div>
-          <div className="col-12 col-lg-8 offset-lg-2 my-5">
-            <div className="row">
-              <div className="col-12 col-md-6 border-end p-5 text-left">
-                <img
-                  src="https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/561/1056141_PE848273_S4.webp"
-                  width="350px"
-                  alt="sofa"
-                />
-              </div>
-              <div className="col-12 col-md-6 p-5 text-left">
-                <div className="form-signin m-auto">
-                  <form id="login-form">
-                    <h1 className="h3 mb-3 display-1">
-                      Log in to your account
-                    </h1>
-                    <span>Please enter your email and password</span>
-                    <div className="mb-3 mt-3">
-                      <div className="d-flex justify-content-between">
-                        <label htmlFor="login-email">Email</label>
-                        <label className="text-danger text-end fw-bold">
-                          *
-                        </label>
-                      </div>
-                      <input
-                        type="email"
-                        className="form-control"
-                        id="login-email"
-                        placeholder="Enter email address ..."
-                        autoComplete="off"
-                        required=""
-                      />
-                    </div>
-                    <div className="mb-4">
-                      <div className="d-flex justify-content-between">
-                        <label htmlFor="login-password">Password</label>
-                        <label className="text-danger text-end fw-bold">
-                          *
-                        </label>
-                      </div>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="login-password"
-                        placeholder="Enter your password ..."
-                        autoComplete="off"
-                        required=""
-                      />
-                    </div>
-                    <button
-                      className="btn btn-lg btn-primary rounded-pill w-100 p-2"
-                      type="submit"
-                    >
-                      Log In
-                    </button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* End Login Section */}
+
+      <Login />
+
       {/* Home Section */}
       <section className="container-fluid" id="home-section">
         <div className="row">
