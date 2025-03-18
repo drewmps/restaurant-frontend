637a5e56519600fc3a643b12c39d63a71e05eda2
drewmps <andrewmarco97@gmail.com>
2025-03-18 15:32:06 +0700

    refactor: memisahkan preloader menjadi component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
+import Preloader from "./components/Preloader";
 import DetailProduct from "./pages/DetailProduct";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      {/* Preloader */}
-      <div id="preloader" style={{ display: "none" }}>
-        <div className="loading">
-          <lottie-player
-            src="https://assets2.lottiefiles.com/packages/lf20_remmdtqv.json"
-            background="transparent"
-            speed={1}
-            style={{ width: 300, height: 300 }}
-            loop=""
-            autoPlay=""
-          />
-        </div>
-      </div>
-      {/* End Preloader */}
+      <Preloader />
+
       {/* Navbar */}
       <header
         className="navbar sticky-top bg-white flex-md-nowrap p-0 shadow"
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,21 @@
+function Preloader() {
+  return (
+    <>
+      {/* Preloader */}
+      <div id="preloader" style={{ display: "none" }}>
+        <div className="loading">
+          <lottie-player
+            src="https://assets2.lottiefiles.com/packages/lf20_remmdtqv.json"
+            background="transparent"
+            speed={1}
+            style={{ width: 300, height: 300 }}
+            loop=""
+            autoPlay=""
+          />
+        </div>
+      </div>
+      {/* End Preloader */}
+    </>
+  );
+}
+export default Preloader;
