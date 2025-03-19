abfed1815b670d8e68e28d0b10357494ab720eab
drewmps <andrewmarco97@gmail.com>
2025-03-19 18:00:36 +0700

    refactor: memisahkan sidebar menjadi component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Preloader from "./components/Preloader";
+import Sidebar from "./components/Sidebar";
 import AddStaff from "./pages/AddStaff";
 import Categories from "./pages/Categories";
 import CreateProduct from "./pages/CreateProduct";
@@ -21,62 +22,7 @@ function App() {
       {/* Home Section */}
       <section className="container-fluid" id="home-section">
         <div className="row">
-          {/* Sidebar */}
-          <nav
-            className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-            id="sidebar-menu"
-          >
-            <div className="position-sticky pt-5">
-              <ul className="nav flex-column">
-                <li className="nav-item">
-                  <a className="nav-link" href="" id="nav-product">
-                    <span className="icon material-symbols-outlined me-2">
-                      shopping_bag
-                    </span>
-                    Products
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="" id="nav-category">
-                    <span className="icon material-symbols-outlined me-2">
-                      category
-                    </span>
-                    Categories
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="" id="nav-category">
-                    <span className="icon material-symbols-outlined me-2">
-                      account_circle
-                    </span>
-                    Add User
-                  </a>
-                </li>
-              </ul>
-              <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-                <span>Account</span>
-              </h6>
-              <ul className="nav flex-column mb-2">
-                <li className="nav-item">
-                  <a className="nav-link">
-                    <span className="icon material-symbols-outlined me-2">
-                      person
-                    </span>
-                    Hej, <span id="username">Hacktiv8!</span>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="" id="nav-logout">
-                    <span className="icon material-symbols-outlined me-2">
-                      logout
-                    </span>
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          {/* End Sidebar */}
+          <Sidebar />
 
           <Home />
 
