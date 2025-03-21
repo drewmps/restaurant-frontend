19371a18199e8aab21c36d893677b5ab17e2b23b
drewmps <andrewmarco97@gmail.com>
2025-03-21 10:47:30 +0700

    feat: mengambil dan menampilkan cuisines di halaman dashboard admin

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,34 @@
+import axios from "axios";
+import { Link } from "react-router";
+import { getBaseURL } from "../helpers/api";
+import { useEffect, useState } from "react";
+import { formatRupiah } from "../helpers/formatRupiah";
+
 function Products() {
+  const [cuisines, setCuisines] = useState([]);
+  async function fetchCuisine() {
+    try {
+      const response = await axios.get(
+        getBaseURL() + "/apis/restaurant-app/cuisines",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        }
+      );
+      console.log(response.data.data);
+      setCuisines(response.data.data);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: error.response.data.statusCode,
+        text: error.response.data.error,
+      });
+    }
+  }
+  useEffect(() => {
+    fetchCuisine();
+  }, []);
   return (
     <>
       {/* Product Section */}
@@ -8,10 +38,12 @@ function Products() {
       >
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 className="display-2">Products</h1>
-          <button className="btn btn-primary rounded-pill" id="new-product">
-            <span className="icon material-symbols-outlined">add</span>New
-            Product
-          </button>
+          <Link to="/admin/create">
+            <button className="btn btn-primary rounded-pill" id="new-product">
+              <span className="icon material-symbols-outlined">add</span>New
+              Product
+            </button>
+          </Link>
         </div>
         <div className="row">
           <div className="col-12 table-responsive">
@@ -33,138 +65,40 @@ function Products() {
                 </tr>
               </thead>
               <tbody id="table-product">
-                <tr>
-                  <td scope="row">#1</td>
-                  <td className="fw-bold">MILLBERGET</td>
-                  <td>
-                    <img
-                      src="https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/201/1020142_PE831799_S4.jpg"
-                      className="img-fluid"
-                    />
-                  </td>
-                  <td>Swivel chair, murum black</td>
-                  <td>13</td>
-                  <td className="fw-bold">Rp1.599.000</td>
-                  <td>admin@mail.com</td>
-                  <td>
-                    <span className="d-flex">
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          delete
+                {cuisines.map((cuisine, index) => {
+                  return (
+                    <tr key={cuisine.id}>
+                      <td scope="row">#{index + 1}</td>
+                      <td className="fw-bold">{cuisine.name}</td>
+                      <td>
+                        <img src={cuisine.imgUrl} className="img-fluid" />
+                      </td>
+                      <td>{cuisine.description}</td>
+                      <td>{cuisine.stock}</td>
+                      <td className="fw-bold">{formatRupiah(cuisine.price)}</td>
+                      <td>{cuisine.User.username}</td>
+                      <td>
+                        <span className="d-flex">
+                          <a href="" className="ms-3">
+                            <span className="icon material-symbols-outlined text-danger">
+                              delete
+                            </span>
+                          </a>
+                          <a href="" className="ms-3">
+                            <span className="icon material-symbols-outlined text-danger">
+                              edit
+                            </span>
+                          </a>
+                          <a href="" className="ms-3">
+                            <span className="icon material-symbols-outlined text-danger">
+                              image
+                            </span>
+                          </a>
                         </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          edit
-                        </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          image
-                        </span>
-                      </a>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td scope="row">#2</td>
-                  <td className="fw-bold">MICKE</td>
-                  <td>
-                    <img
-                      src="https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/359/0735981_PE740299_S4.jpg"
-                      className="img-fluid"
-                    />
-                  </td>
-                  <td>Desk, black-brown, 105x50 cm</td>
-                  <td>30</td>
-                  <td className="fw-bold">Rp1.799.000</td>
-                  <td>admin@mail.com</td>
-                  <td>
-                    <span className="d-flex">
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          delete
-                        </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          edit
-                        </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          image
-                        </span>
-                      </a>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td scope="row">#3</td>
-                  <td className="fw-bold">ALEX</td>
-                  <td>
-                    <img
-                      src="https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/777/0977775_PE813763_S3.jpg"
-                      className="img-fluid"
-                    />
-                  </td>
-                  <td>drawer unit, white, 36x70 cm</td>
-                  <td>12</td>
-                  <td className="fw-bold">Rp1.370.000</td>
-                  <td>admin@mail.com</td>
-                  <td>
-                    <span className="d-flex">
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          delete
-                        </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          edit
-                        </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          image
-                        </span>
-                      </a>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td scope="row">#4</td>
-                  <td className="fw-bold">SOCKERBIT</td>
-                  <td>
-                    <img
-                      src="https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/290/1029026_PE835627_S3.jpg"
-                      className="img-fluid"
-                    />
-                  </td>
-                  <td>Storage box with lid, grey-green, 19x26x15 cm</td>
-                  <td>42</td>
-                  <td className="fw-bold">Rp59.900</td>
-                  <td>admin@mail.com</td>
-                  <td>
-                    <span className="d-flex">
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          delete
-                        </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          edit
-                        </span>
-                      </a>
-                      <a href="" className="ms-3">
-                        <span className="icon material-symbols-outlined text-danger">
-                          image
-                        </span>
-                      </a>
-                    </span>
-                  </td>
-                </tr>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
