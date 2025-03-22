b76d110abf139c13cd8424650e3fa02e2f20f88c
drewmps <andrewmarco97@gmail.com>
2025-03-22 22:00:26 +0700

    fix: memunculkan image dari cuisine yang imagenya akan diupdate

--- src/pages/UpdateImage.jsx
+++ src/pages/UpdateImage.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import Button from "../components/Button";
 import { getBaseURL } from "../helpers/api";
 import { useNavigate, useParams } from "react-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import NoImage from "../assets/no-image.jpg";
 
 export default function UpdateImage() {
   const { id } = useParams();
@@ -35,6 +36,33 @@ export default function UpdateImage() {
       });
     }
   };
+
+  const [name, setName] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  async function fetchCuisine() {
+    try {
+      const { data } = await axios.get(
+        getBaseURL() + `/apis/restaurant-app/cuisines/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        }
+      );
+      setName(data.data.name);
+      setImgUrl(data.data.imgUrl);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: error.response.data.statusCode,
+        text: error.response.data.error,
+      });
+    }
+  }
+  useEffect(() => {
+    fetchCuisine();
+  }, []);
+
   return (
     <>
       {/* Update Section */}
@@ -46,7 +74,14 @@ export default function UpdateImage() {
           <div className="col-12 col-md-6">
             <div className="pt-3 pb-2 mb-3">
               <form id="register-form" onSubmit={handleSubmit}>
-                <h1 className="h3 mb-3 display-1">Update Image</h1>
+                <h1 className="h3 mb-3 display-1">{name}</h1>
+                <div className="mb-3">
+                  <img
+                    src={imgUrl ? imgUrl : NoImage}
+                    style={{ width: 250, height: "100%" }}
+                    alt="Cuisine Image"
+                  />
+                </div>
                 {/* <div class="mb-3"> */}
                 <div className="input-group mb-3">
                   <input
